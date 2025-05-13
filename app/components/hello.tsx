5dc0204fa1c22f6a62b38b65265ef13f806da359
Moris Mwai <mwaimoris@gmail.com>
2025-05-13 23:53:32 +0300

    playground set up and configuration
    Signed-off-by: Moris Mwai <mwaimoris@gmail.com>

---
+++ app/components/hello.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+function Hello() {
+    console.log("I am a client component");
+
+    return (
+        <div>
+            <h1>Hello, Next.js!</h1>
+            <p>This is a client component.</p>
+        </div>
+    );
+}
+
+export default Hello;
---
+++ app/dashboard/users/[id]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Page = ({ params}: { params: { id: string }}) => {
+    const { id } = params;
+    return (
+        <h1 className="2xl">User Profile: {id}</h1>
+    )
+}
+export default Page
